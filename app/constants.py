fb89581a79a746645da5656960c725fbe29d9758
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-08-11 10:56:32 -0300

    refact: pasta de tasks movida para app

--- app/constants.py
+++ app/constants.py
@@ -1,67 +1,98 @@
 PRODUCT_MAPPING = {
-    
-    "Relatório de Acompanhamento Hidrológico": 
-    {"task": "vazoes_observadas"},
-    "Precipitação por Satélite.": 
-    {"task": "psat_file"},
-    "Modelo GEFS": 
-    {"task": "modelo_gefs"},
-    "Resultados preliminares não consistidos  (vazões semanais - PMO)": 
-    {"task": "resultados_nao_consistidos_semanal"},
-    "Relatório dos resultados finais consistidos da previsão diária (PDP)": 
-    {"task": "relatorio_resutados_finais_consistidos"},
-    "Níveis de Partida para o DESSEM": 
-    {"task": "niveis_partida_dessem"},
-    "DADVAZ – Arquivo de Previsão de Vazões Diárias (PDP)": 
-    {"task": "dadvaz_vaz_prev"},
-    "Deck e Resultados DECOMP - Valor Esperado": 
-    {"task": "deck_resultados_decomp"},
-    "Resultados finais consistidos (vazões diárias - PDP)": 
-    {"task": "resultados_finais_consistidos"},
-    "Resultados preliminares consistidos (vazões semanais - PMO)": 
-    {"task": "resultados_preliminares_consistidos"},
-    "Arquivos dos modelos de previsão de vazões semanais - PMO": 
-    {"task": "entrada_saida_previvaz"},
-    "Arquivos dos modelos de previsão de vazões diárias - PDP": 
-    {"task": "arquivos_modelo_pdp"},
-    "Acomph": 
-    {"task": "arquivo_acomph"},
-    "RDH": 
-    {"task": "arquivo_rdh"},
-    "Histórico de Precipitação por Satélite": 
-    {"task": "historico_preciptacao"},
-    "Modelo ETA": 
-    {"task": "modelo_eta"},
-    "Carga por patamar - DECOMP": 
-    {"task": "carga_patamar"},
-    "Deck Preliminar DECOMP - Valor Esperado": 
-    {"task": "deck_preliminar_decomp"},
-    "Decks de entrada e saída - Modelo DESSEM": 
-    {"task": "deck_entrada_saida_dessem"},
-    "Arquivos de Previsão de Carga para o DESSEM": 
-    {"task": "previsao_carga_dessem"},
-    "Decks de entrada do PrevCargaDESSEM": 
-    {"task": "prevCarga_dessem"},
-    "Previsões de carga mensal e por patamar - NEWAVE": 
-    {"task": "carga_patamar_nw"},
-    "IPDO (Informativo Preliminar Diário da Operação)": 
-    {"task": "carga_IPDO"},
-    "Modelo ECMWF": 
-    {"task": "modelo_ECMWF"},
-    "Dados utilizados na previsão de geração eólica": 
-    {"task": "dados_geracaoEolica"},
-    "Arquivos de Previsão de Carga para o DESSEM - PrevCargaDESSEM": 
-    {"task": "prevCarga_dessem_saida"},
-    'Deck NEWAVE Preliminar': 
-    {"task": "carga_newave_preliminar"},
-    'DECK NEWAVE DEFINITIVO': 
-    {"task": "DECKS_NEWAVE"}, 
-    "DECKS DA PREVISÃO DE GERAÇÃO EÓLICA SEMANAL WEOL-SM": 
-    {"task": "deck_prev_eolica_semanal_weol"},
-    "Preliminar - Relatório Mensal de Limites de Intercâmbio": 
-    {"task": "relatorio_limites_intercambio"},
-    "Relatório Mensal de Limites de Intercâmbio para o Modelo DECOMP": 
-    {"task": "relatorio_limites_intercambio"},
-    "Notas Técnicas - Medio Prazo": 
-    {"task": "notas_tecnicas_medio_prazo"},
+   "Relatório de Acompanhamento Hidrológico":{
+      "task":"vazoes_observadas"
+   },
+   "Precipitação por Satélite.":{
+      "task":"psat_file"
+   },
+   "Modelo GEFS":{
+      "task":"modelo_gefs"
+   },
+   "Resultados preliminares não consistidos  (vazões semanais - PMO)":{
+      "task":"resultados_nao_consistidos_semanal"
+   },
+   "Relatório dos resultados finais consistidos da previsão diária (PDP)":{
+      "task":"relatorio_resutados_finais_consistidos"
+   },
+   "Níveis de Partida para o DESSEM":{
+      "task":"niveis_partida_dessem"
+   },
+   "DADVAZ – Arquivo de Previsão de Vazões Diárias (PDP)":{
+      "task":"dadvaz_vaz_prev"
+   },
+   "Deck e Resultados DECOMP - Valor Esperado":{
+      "task":"deck_resultados_decomp"
+   },
+   "Resultados finais consistidos (vazões diárias - PDP)":{
+      "task":"resultados_finais_consistidos"
+   },
+   "Resultados preliminares consistidos (vazões semanais - PMO)":{
+      "task":"resultados_preliminares_consistidos"
+   },
+   "Arquivos dos modelos de previsão de vazões semanais - PMO":{
+      "task":"entrada_saida_previvaz"
+   },
+   "Arquivos dos modelos de previsão de vazões diárias - PDP":{
+      "task":"arquivos_modelo_pdp"
+   },
+   "Acomph":{
+      "task":"arquivo_acomph"
+   },
+   "RDH":{
+      "task":"arquivo_rdh"
+   },
+   "Histórico de Precipitação por Satélite":{
+      "task":"historico_preciptacao"
+   },
+   "Modelo ETA":{
+      "task":"modelo_eta"
+   },
+   "Carga por patamar - DECOMP":{
+      "task":"carga_patamar"
+   },
+   "Deck Preliminar DECOMP - Valor Esperado":{
+      "task":"deck_preliminar_decomp"
+   },
+   "Decks de entrada e saída - Modelo DESSEM":{
+      "task":"deck_entrada_saida_dessem"
+   },
+   "Arquivos de Previsão de Carga para o DESSEM":{
+      "task":"previsao_carga_dessem"
+   },
+   "Decks de entrada do PrevCargaDESSEM":{
+      "task":"prevCarga_dessem"
+   },
+   "Previsões de carga mensal e por patamar - NEWAVE":{
+      "task":"carga_patamar_nw"
+   },
+   "IPDO (Informativo Preliminar Diário da Operação)":{
+      "task":"carga_IPDO"
+   },
+   "Modelo ECMWF":{
+      "task":"modelo_ECMWF"
+   },
+   "Dados utilizados na previsão de geração eólica":{
+      "task":"dados_geracaoEolica"
+   },
+   "Arquivos de Previsão de Carga para o DESSEM - PrevCargaDESSEM":{
+      "task":"prevCarga_dessem_saida"
+   },
+   "Deck NEWAVE Preliminar":{
+      "task":"carga_newave_preliminar"
+   },
+   "DECK NEWAVE DEFINITIVO":{
+      "task":"DECKS_NEWAVE"
+   },
+   "DECKS DA PREVISÃO DE GERAÇÃO EÓLICA SEMANAL WEOL-SM":{
+      "task":"deck_prev_eolica_semanal_weol"
+   },
+   "Preliminar - Relatório Mensal de Limites de Intercâmbio":{
+      "task":"relatorio_limites_intercambio"
+   },
+   "Relatório Mensal de Limites de Intercâmbio para o Modelo DECOMP":{
+      "task":"relatorio_limites_intercambio"
+   },
+   "Notas Técnicas - Medio Prazo":{
+      "task":"notas_tecnicas_medio_prazo"
+   }
 }
