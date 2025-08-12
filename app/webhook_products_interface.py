0e82daf3aae87f5bbdb2e0386ecb6f357e6a18da
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-08-12 13:51:25 -0300

    refact: mudando nome de variaveis e traduzindo doc

--- app/webhook_products_interface.py
+++ app/webhook_products_interface.py
@@ -6,49 +6,30 @@ logger = setup_logger()
 
 class WebhookProductsInterface(ABC):
     """
-    Interface for webhook products.
-    This interface defines the methods that must be implemented by any class that handles webhook products.
+    Interface para os produtos do webhook ONS.
+    Define os metodos base que devem ser implementados no ETL dos produtos do webhook.
     """
 
     @abstractmethod
     def run_workflow(self):
         """
-        Execute the flow for processing webhook products.
-        This method should be implemented to handle the specific logic for processing webhook products.
+        Ponto de entrada da execucao do etl.
+        Deve implementar a logica de execucao para cada produto.
         """
         pass
 
-    def get_product_details(self):
-        """
-        Retrieves product details from the configuration.
-        
-        :param conf: Configuration dictionary containing product details.
-        :return: A dictionary with product details.
-        """
-        try:
-            if not self.conf or 'product_details' not in self.conf:
-                raise ValueError("Configuração inválida: configuração ou 'product' não encontrado.")
-
-            product_details = self.conf['product_details']
-            
-            logger.info("Payload do DECK Newave recebido com sucesso!")
-            
-            return product_details
-        
-        except Exception as e:
-            logger.error(f"Erro em obter o payload do DECK Newave: {e}")
     
-    def download_files(self, product_details):
+    def download_files(self, payload_webhook: dict):
         """
         Download files related to the webhook product.
         This method should implement the logic for downloading necessary files based on product details.
         
-        :param product_details: Details of the product to download files for.
+        :param payload_webhook: Details of the product to download files for.
         :return: A dictionary with the status and message of the download operation.
         """
         
-        id_produto = product_details.get('webhookId')
-        file_name = product_details.get('filename')
+        id_produto = payload_webhook.get('webhookId')
+        file_name = payload_webhook.get('filename')
         path_download = "/tmp"
         
         try: 
@@ -57,4 +38,3 @@ class WebhookProductsInterface(ABC):
             logger.error(f"Erro ao baixar arquivo do S3: {e}")
             return {"status": "error", "message": str(e)}
         
-        pass
