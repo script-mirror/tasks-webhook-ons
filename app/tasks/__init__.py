c2dce741d4b4b2126d33c7871822bcff4f395360
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-09-08 14:50:00 -0300

    feat: mapeando newave

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,7 +1,11 @@
 from .relatorio_limites_intercambio_modelo_decomp import RelatorioLimitesIntercambioDecomp
 from .carga_patamar_decomp import CargaPatamarDecomp
+from .previsoes_carga_mensal_patamar_newave_read import CargaPatamarNewave
+from .decks_newave import DecksNewave
 
 __all__ = [
     "RelatorioLimitesIntercambioDecomp",
     "CargaPatamarDecomp",
+    "CargaPatamarNewave",
+    "DecksNewave",
 ]
