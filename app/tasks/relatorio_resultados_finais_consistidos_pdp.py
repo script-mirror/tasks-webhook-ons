4604a6e18cf725154fa51a5f5a26f8593dfd0b73
Polastrine <diogopolastrineds@gmail.com>
2025-10-02 15:45:07 -0300

    feat: processo geracao produto resultado preliminares nao consistidos e import do produto relatorio resultados finais

--- app/tasks/relatorio_resultados_finais_consistidos_pdp.py
+++ app/tasks/relatorio_resultados_finais_consistidos_pdp.py
@@ -6,6 +6,7 @@ import pdb
 import sys
 import glob
 import os
+import requests
 
 current_file = Path(__file__).resolve()
 project_root = current_file.parent.parent.parent
@@ -15,6 +16,9 @@ from app.schema import WebhookSintegreSchema
 
 from middle.utils import setup_logger, Constants
 from middle.utils.file_manipulation import extract_zip
+from middle.utils import ( # noqa: E402
+    get_auth_header
+)
 logger = setup_logger()
 constants = Constants()
 
@@ -23,6 +27,9 @@ class RelatorioResultadosFinaisConsistidosPDP(WebhookProductsInterface):
     
     def __init__(self, payload: Optional[WebhookSintegreSchema]):
         super().__init__(payload)
+        self.gerador_tabela = GeradorTabela()
+        self.headers = get_auth_header()
+        self.consts = Constants()
         
         
     def run_workflow(self):
@@ -39,26 +46,100 @@ class RelatorioResultadosFinaisConsistidosPDP(WebhookProductsInterface):
         
     def run_process(self, file_path):
         
-        self.process_file(file_path)
+        df_load = self.process_file(file_path)
         
-        self.post_data(None)
+        self.post_data(df_load)
         
-        self.gerador_figuras.run_process()
+        # self.gerador_tabela.run_process(df_load)
         
         pass
     
     def process_file(self, file_path):
         logger.info("Processando arquivos do produto... Arquivo encontrado: %s", file_path)
         try:
-            pass
+            filepath_splited = os.path.basename(file_path).split('_')
+            dia_previsao = int(filepath_splited[3])
+            mes_pevisao = int(filepath_splited[4])
+            ano_previsao = int(filepath_splited[5])
+            
+            dt_previsao = datetime.datetime(ano_previsao, mes_pevisao, dia_previsao)
+            
+            info_planilha = {
+                'SUDESTE': {'sheet_name':'Diária_6', 'posicao_info':[168, 169]},
+                'SUL': {'sheet_name':'Diária_7', 'posicao_info':[56, 57]},
+                'NORDESTE': {'sheet_name':'Diária_7', 'posicao_info':[70, 71]},
+                'NORTE': {'sheet_name':'Diária_7', 'posicao_info':[125, 126]},
+            }
+            
+            cd_submercado = {'SUDESTE':1, 'SUL':2, 'NORDESTE':3, 'NORTE':4}
+            
+            values_to_insert = []
+            for submercado in info_planilha.keys():
+                df_load = pd.read_excel(file_path, sheet_name=info_planilha[submercado]['sheet_name'], skiprows=4)
+            
+                colunas = df_load.columns.tolist()
+                
+                index_submercado = df_load[df_load[colunas[4]]==submercado].index[0]
+                
+                df_load = df_load.loc[index_submercado+1:index_submercado+2][colunas[2:]]
+                
+                df_load.columns = pd.to_datetime(df_load.columns).strftime("%Y-%m-%d")
+                
+                df_load = df_load.T
+                
+                df_load = df_load.reset_index()
+                
+                df_load['cd_submercado'] = cd_submercado[submercado]
+                
+                df_load['dt_previsao'] = dt_previsao.strftime("%Y-%m-%d")
+                
+                df_load.columns.values[0] = 'dt_ref'
+                
+                df_load.columns.values[1] = 'vl_mwmed'
+                
+                df_load.columns.values[2] = 'vl_perc_mlt'
+                
+                df_load = df_load[['cd_submercado', 'dt_previsao', 'dt_ref', 'vl_mwmed', 'vl_perc_mlt']]
+                
+                values_to_insert.append(df_load)
+                
+            df_load = pd.concat(values_to_insert, ignore_index=True) 
+            
+            return df_load
             
         except Exception as e:
             logger.error("Falha em processar os arquivos do produto: %s", str(e), exc_info=True)
             raise
         
     def post_data(self, process_result: pd.DataFrame) -> dict:
-        logger.info("Produto sem importação de dados.")
+        try:
+            res = requests.post(
+                self.consts.POST_PREV_ENA,
+                json=process_result.to_dict('records'),
+                headers=self.headers
+            )
+            if res.status_code != 200:
+                res.raise_for_status()
+                    
+        except Exception as e:
+            raise
+    
+
+class GeradorTabela:
+    def __init__(self):
         pass
+    
+    def run_process(self):
+        pass
+    
+    
+    def gerar_html(self):
+        pass
+    
+    def enviar_email(self, assunto: str, corpo: str, arquivos: list):
+        pass
+    
+    
     
 
 
@@ -66,23 +147,23 @@ if __name__ == '__main__':
     logger.info("Iniciando manualmente o workflow do produto Precipitação por Satélite - ONS...")
     try:
         payload = {
-            "dataProduto": "24/09/2025",
-            "filename": "psat_24092025.txt",
-            "macroProcesso": "Programação da Operação",
-            "nome": "Precipitação por Satélite – ONS",
-            "periodicidade": "2025-09-24T00:00:00",
-            "periodicidadeFinal": "2025-09-24T23:59:59",
-            "processo": "Meteorologia e clima",
-            "s3Key": "webhooks/Precipitação por Satélite – ONS/68d42644450014d70a3e6056_psat_24092025.txt",
-            "url": "https://apps08.ons.org.br/ONS.Sintegre.Proxy/webhook?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVUkwiOiJodHRwczovL3NpbnRlZ3JlLm9ucy5vcmcuYnIvc2l0ZXMvOS8zOC9Qcm9kdXRvcy80ODcvcHNhdF8yNDA5MjAyNS50eHQiLCJ1c2VybmFtZSI6ImdpbHNldS5tdWhsZW5AcmFpemVuLmNvbSIsIm5vbWVQcm9kdXRvIjoiUHJlY2lwaXRhw6fDo28gcG9yIFNhdMOpbGl0ZSDigJMgT05TIiwiSXNGaWxlIjoiVHJ1ZSIsImlzcyI6Imh0dHA6Ly9sb2NhbC5vbnMub3JnLmJyIiwiYXVkIjoiaHR0cDovL2xvY2FsLm9ucy5vcmcuYnIiLCJleHAiOjE3NTg4MjA1MzEsIm5iZiI6MTc1ODczMzg5MX0.2mgFER2IWoeq20j5_iarX3-Df9935DJOoj_1F8peL-E",
-            "webhookId": "68d42644450014d70a3e6056"
-        }
+  "dataProduto": "30/09/2025",
+  "filename": "Relatorio_previsao_diaria_28_09_2025_para_30_09_2025.xls",
+  "macroProcesso": "Programação da Operação",
+  "nome": "Relatório dos resultados finais consistidos da previsão diária (PDP)",
+  "periodicidade": "2025-09-30T00:00:00",
+  "periodicidadeFinal": "2025-09-30T23:59:59",
+  "processo": "Previsão de Vazões Diárias - PDP",
+  "s3Key": "webhooks/Relatório dos resultados finais consistidos da previsão diária (PDP)/68d98616995e02e90c3fc514_Relatorio_previsao_diaria_28_09_2025_para_30_09_2025.xls",
+  "url": "https://apps08.ons.org.br/ONS.Sintegre.Proxy/webhook?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVUkwiOiJodHRwczovL3NpbnRlZ3JlLm9ucy5vcmcuYnIvc2l0ZXMvOS8xMy84Mi9Qcm9kdXRvcy81NDgvUmVsYXRvcmlvX3ByZXZpc2FvX2RpYXJpYV8yOF8wOV8yMDI1X3BhcmFfMzBfMDlfMjAyNS54bHMiLCJ1c2VybmFtZSI6ImdpbHNldS5tdWhsZW5AcmFpemVuLmNvbSIsIm5vbWVQcm9kdXRvIjoiUmVsYXTDs3JpbyBkb3MgcmVzdWx0YWRvcyBmaW5haXMgY29uc2lzdGlkb3MgZGEgcHJldmlzw6NvIGRpw6FyaWEgKFBEUCkiLCJJc0ZpbGUiOiJUcnVlIiwiaXNzIjoiaHR0cDovL2xvY2FsLm9ucy5vcmcuYnIiLCJhdWQiOiJodHRwOi8vbG9jYWwub25zLm9yZy5iciIsImV4cCI6MTc1OTE3Mjc0MiwibmJmIjoxNzU5MDg2MTAyfQ.sQMIZ57iv4NHLxfcWGTMA9Mnh_8yiiRxtbXF64qIc6Q",
+  "webhookId": "68d98616995e02e90c3fc514"
+}
         
         payload = WebhookSintegreSchema(**payload)
         
-        preciptacao_satelite = PrecipitacaoPorSateliteONS(payload)
+        relatorio_resultados = RelatorioResultadosFinaisConsistidosPDP(payload)
         
-        preciptacao_satelite.run_workflow()
+        relatorio_resultados.run_workflow()
         
 
     except Exception as e:
--- app/tasks/resultados_preliminares_nao_consistidos.py
+++ app/tasks/resultados_preliminares_nao_consistidos.py
@@ -5,6 +5,8 @@ import pdb
 import sys
 import glob
 import os
+import re
+import numpy as np
 
 current_file = Path(__file__).resolve()
 project_root = current_file.parent.parent.parent
@@ -12,8 +14,10 @@ sys.path.insert(0, str(project_root))
 from app.webhook_products_interface import WebhookProductsInterface
 from app.schema import WebhookSintegreSchema
 
-from middle.utils import setup_logger, Constants
+from middle.utils import setup_logger, Constants, html_to_image
 from middle.utils.file_manipulation import extract_zip
+from middle.message.sender import send_email_message, send_whatsapp_message
+from middle.airflow import trigger_dag
 logger = setup_logger()
 constants = Constants()
 
@@ -22,6 +26,9 @@ class ResultadosPreliminaresNaoConsistidos(WebhookProductsInterface):
     
     def __init__(self, payload: Optional[WebhookSintegreSchema]):
         super().__init__(payload)
+        self.gerador_tabela = GeradorTabela()
+        self.trigger_dag = trigger_dag
+
         
         
     def run_workflow(self):
@@ -37,48 +44,193 @@ class ResultadosPreliminaresNaoConsistidos(WebhookProductsInterface):
             raise
         
     def run_process(self, file_path):
-        process_result = self.process_file(file_path)
         
-        self.post_data(process_result)
+        df_load, rev, assunto = self.process_file(file_path)
         
-        pass
+        self.gerador_tabela.run_process(df_load, rev, assunto)
+        
+        month_match = re.search(r'_(\d{6})_', os.path.basename(file_path))
+
+        if month_match:
+        
+            payload = {key: self.payload.dict().get(key) for key in self.payload.dict().keys()}
+            payload['sensibilidade'] = "NAO_CONSISTIDO"
+
+            self.trigger_dag(dag_id="1.12-PROSPEC_CONSISTIDO", conf=payload)
     
     def process_file(self, file_path):
         logger.info("Processando arquivos do produto... Arquivo encontrado: %s", file_path)
         try:
+            
             unzip_path = extract_zip(file_path)
             logger.debug("Arquivo extraído para: %s", unzip_path)
             
-            load_path = glob.glob(os.path.join(unzip_path, '*xlsx'))[0]
+            load_path = glob.glob(os.path.join(unzip_path, "*"))[0]
             
-            df_load = pd.read_excel(load_path)
+            files = os.listdir(load_path)
+            
+            for f in files:
+                if '.xls' in f:
+                    load_path = os.path.join(load_path, f)
+                    break
+            
+            rev = file_path.split('_')[-1].split('.')[0]
+            
+            assunto = file_path.split('/')[-1].split('.')[0]
+            
+            configs = {}
+            if rev == 'PMO':
+                configs = {'sheet': 'Tab-5-6-7', 'rows': 10, 'skip': 3, 'x': 1}
+            else:
+                configs = {'sheet': 'REV-2', 'rows': 11, 'skip': 4, 'x': 2}
+                
+            df_load = pd.read_excel(load_path, sheet_name=configs['sheet'],nrows = configs['rows']-configs['skip']+1,skiprows=configs['skip'], header=None)
+            
+            df_load = df_load.replace(np.nan, '', regex=True)
+
+            df_load.at[0, configs['x']] = str(df_load.loc[0][configs['x']].strftime('%d/%m/%Y'))
+            df_load.at[2, configs['x']] = str(df_load.loc[2][configs['x']].strftime('%d/%m/%Y'))
+            
+            rev = file_path.split('_')[-1].split('.')[0]
+            
+            return [df_load, rev, assunto]
+            
         except Exception as e:
             logger.error("Falha em processar os arquivos do produto: %s", str(e), exc_info=True)
             raise
-        
         
     def post_data(self, process_result: pd.DataFrame) -> dict:
         pass
     
 
+class GeradorTabela:
+    def __init__(self):
+        self.user = constants.EMAIL_REV_ENA
+        self.destinatarios_email = [constants.EMAIL_MIDDLE, constants.EMAIL_FRONT]
+        self.destinatario_whatsapp = constants.WHATSAPP_PREMISSAS_PRECO
+        
+        
+    def run_process(self, df_load: pd.DataFrame, rev: str, assunto: str):
+        logger.info("Gerando tabela e enviando para os canais de comunicação...")
+        try:
+
+            html = self.gerar_html(df_load, rev)   
+
+            self.enviar_email(html, assunto)
+
+            self.enviar_whatsapp(html, assunto)
+            
+            logger.info("Finalizado o processo de geração e envio da tabela.")
+            
+        except Exception as e:
+            logger.error("Erro no processamento de geracao da tabela: %s", str(e), exc_info=True)
+            raise
+        
+        
+    def gerar_html(self, df_load: pd.DataFrame, rev: str):
+        try:
+            
+            rev = rev
+
+            html = '''<html><head>'''
+            html = html + ''' <style type="text/css">'''
+            html = html + '''table {font-family: arial, sans-serif;border-collapse: collapse;width: 1000px;}'''
+            html = html + '''td, th {border: 1px solid #dddddd;text-align: center;padding: 8px;}'''
+            html = html + '''tr:nth-child(4n + 2) td, tr:nth-child(4n + 3) td {background-color: #F2ECEC;}'''
+            html = html + '''.umaLinha tr:nth-child(odd) td{ background-color: #F2ECEC;}'''
+            html = html + '''.umaLinha tr:nth-child(even) td{ background-color: #ffffff;}'''
+            html = html + '''</style></head><body>'''
+            html += '<table class="umaLinha" class="dataframe"><tbody><tr>'
+            html += '<th rowspan="2">REGIOES</th>'
+
+            if rev == 'PMO':
+                html += '<th colspan="2">'+df_load.loc[0][1]+'<br> a <br>'+df_load.loc[2][1]+'</th>'
+                html += '<th colspan="2">Previsao<br>Mensal Inicial<br>'+df_load.loc[2][3]+'</th></tr>'
+                html += '<tr><td>(Mwmed)</td><td>% MLT</td><td>(Mwmed)</td><td>MLT%</td></tr>'
+            else:
+                html += '<th colspan="2">'+df_load.loc[0][2]+'<br> a <br>'+df_load.loc[2][2]+'</th>'
+                html += '<th colspan="2">Revisao da<br>Previsao Mensal<br>Incluindo Verificado</th>'
+                html += '<th colspan="2">Previsao<br>Mensal Inicial<br>'+df_load.loc[2][6]+'</th></tr>'
+                html += '<tr><td>(Mwmed)</td><td>% MLT</td><td>(Mwmed)</td><td>MLT%</td><td>(Mwmed)</td><td>% MLT</td></tr>'
+
+
+            for i,row in df_load.iterrows():
+                if i >= 4:
+                    html += '<tr>'
+                    if rev == 'PMO':
+                        html += '<td>{}</td>'.format(row[0])
+                        html += '<td>{:6.0f}</td>'.format(row[1])
+                        html += '<td>{:6.0f}</td>'.format(row[2])
+                        html += '<td>{:6.0f}</td>'.format(row[3])
+                        html += '<td>{:6.0f}</td>'.format(row[4])
+                    else:
+                        html += '<td>{}</td>'.format(row[1])
+                        html += '<td>{:6.0f}</td>'.format(row[2])
+                        html += '<td>{:6.0f}</td>'.format(row[3])
+                        html += '<td>{:6.0f}</td>'.format(row[4])
+                        html += '<td>{:6.0f}</td>'.format(row[5])
+                        html += '<td>{:6.0f}</td>'.format(row[6])
+                        html += '<td>{:6.0f}</td>'.format(row[7])
+
+                    html += '</tr>'
+
+            html = html + '</tbody></table></body> </html>'
+
+            return html
+        
+        except Exception as e:
+            logger.error("Falha ao gerar HTML: %s", str(e), exc_info=True)
+            raise
+
+    def enviar_email(self, html: str, assunto: str):
+        try:
+            
+            send_email_message(
+                user=self.user,
+                destinatario=self.destinatarios_email,
+                mensagem=html,
+                assunto=assunto,
+                arquivos=[]
+            )
+
+            logger.info("E-mail enviado para %s com assunto '%s'.", self.destinatario, assunto)
+            
+        except Exception as e:
+            logger.error("Falha ao enviar e-mail: %s", str(e), exc_info=True)
+            raise
+        
+    def enviar_whatsapp(self, html: str, assunto: str):
+        try:
+            
+            image_binary = html_to_image(html)
+
+            send_whatsapp_message(destinatario=self.destinatario_whatsapp,mensagem=assunto,arquivo=image_binary)
+            
+        except Exception as e:
+            logger.error("Falha ao enviar mensagem WhatsApp: %s", str(e), exc_info=True)
+            raise
+          
 if __name__ == '__main__':
     logger.info("Iniciando manualmente o workflow do produto Resultados Preliminares Nao Consistidos...")
     try:
         payload = {
-            "dataProduto": "24/09/2025",
-            "filename": "psat_24092025.txt",
+            "dataProduto": "20/09/2025 - 26/09/2025",
+            "filename": "Nao_Consistido_202509_REV3.zip",
             "macroProcesso": "Programação da Operação",
-            "nome": "Precipitação por Satélite – ONS",
-            "periodicidade": "2025-09-24T00:00:00",
-            "periodicidadeFinal": "2025-09-24T23:59:59",
-            "processo": "Meteorologia e clima",
-            "s3Key": "webhooks/Precipitação por Satélite – ONS/68d42644450014d70a3e6056_psat_24092025.txt",
-            "url": "https://apps08.ons.org.br/ONS.Sintegre.Proxy/webhook?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVUkwiOiJodHRwczovL3NpbnRlZ3JlLm9ucy5vcmcuYnIvc2l0ZXMvOS8zOC9Qcm9kdXRvcy80ODcvcHNhdF8yNDA5MjAyNS50eHQiLCJ1c2VybmFtZSI6ImdpbHNldS5tdWhsZW5AcmFpemVuLmNvbSIsIm5vbWVQcm9kdXRvIjoiUHJlY2lwaXRhw6fDo28gcG9yIFNhdMOpbGl0ZSDigJMgT05TIiwiSXNGaWxlIjoiVHJ1ZSIsImlzcyI6Imh0dHA6Ly9sb2NhbC5vbnMub3JnLmJyIiwiYXVkIjoiaHR0cDovL2xvY2FsLm9ucy5vcmcuYnIiLCJleHAiOjE3NTg4MjA1MzEsIm5iZiI6MTc1ODczMzg5MX0.2mgFER2IWoeq20j5_iarX3-Df9935DJOoj_1F8peL-E",
-            "webhookId": "68d42644450014d70a3e6056"
+            "nome": "Resultados preliminares não consistidos  (vazões semanais - PMO)",
+            "periodicidade": "2025-09-20T00:00:00",
+            "periodicidadeFinal": "2025-09-26T23:59:59",
+            "processo": "Previsão de Vazões e Geração de Cenários - PMO",
+            "s3Key": "webhooks/Resultados preliminares não consistidos  (vazões semanais - PMO)/68cc6cb792d5b4018cefcb5c_Nao_Consistido_202509_REV3.zip",
+            "url": "https://apps08.ons.org.br/ONS.Sintegre.Proxy/webhook?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVUkwiOiJodHRwczovL3NpbnRlZ3JlLm9ucy5vcmcuYnIvc2l0ZXMvOS8xMy83OS9Qcm9kdXRvcy8yNDYvTmFvX0NvbnNpc3RpZG9fMjAyNTA5X1JFVjMuemlwIiwidXNlcm5hbWUiOiJnaWxzZXUubXVobGVuQHJhaXplbi5jb20iLCJub21lUHJvZHV0byI6IlJlc3VsdGFkb3MgcHJlbGltaW5hcmVzIG7Do28gY29uc2lzdGlkb3MgICh2YXrDtWVzIHNlbWFuYWlzIC0gUE1PKSIsIklzRmlsZSI6IlRydWUiLCJpc3MiOiJodHRwOi8vbG9jYWwub25zLm9yZy5iciIsImF1ZCI6Imh0dHA6Ly9sb2NhbC5vbnMub3JnLmJyIiwiZXhwIjoxNzU4MzE0Mjc5LCJuYmYiOjE3NTgyMjc2Mzl9.0gWgGwt_9zqu5loodmFmYaLX-KvRRStLZ2Ou3yZw8wk",
+            "webhookId": "68cc6cb792d5b4018cefcb5c"
         }
         
         payload = WebhookSintegreSchema(**payload)
         
+        resultados_preliminares = ResultadosPreliminaresNaoConsistidos(payload)
+        
+        resultados_preliminares.run_workflow()
 
     except Exception as e:
         logger.error("Erro no fluxo manual de processamento das Resultados Preliminares Nao Consistidos: %s", str(e), exc_info=True)
